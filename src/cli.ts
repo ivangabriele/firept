fdf0e5256be48ced758ce367c3d73c508b8e1c9b
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-08-19 04:00:49 +0200

    ci(release): v0.2.0

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import { program } from 'commander'
 
 import { server } from './server.js'
 
-program.name('fire').description('FirePT').version('0.1.0')
+program.name('fire').description('FirePT').version('0.2.0')
 
 program.command('start').description('Start FirePT to APIfy your current directory.').action(server.start)
 
